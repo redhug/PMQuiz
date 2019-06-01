c09bf9ca6002a842c9af373f3f7f022976c377e2
sushmareddy <sushmarani.as72>
2019-06-01 14:46:09 -0500

    Changes

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" 
+      style="background-image: url('https://mdbootstrap.com/img/Photos/Others/architecture.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+
+    <div class="row heading_style">
+    <b-alert variant="success" show class="">Risk management questions</b-alert>
+    </div>
+    <HomePage answer="risk-averse" question="1) Risk utility rises at a decreasing rate for a _______ person."/>
+    <HomePage answer="risk-seeking" question="2) Those who are ________ have a higher tolerance for risk, and their satisfaction increases when more payoff is at stake."/>
+    <HomePage answer="risk-neutral" question="3) A ________ person achieves a balance between risk and payoff."/>
+    <HomePage answer="Planning risk management" question="4) _________ involves deciding how to approach and plan the risk management activities for the project."/>
+    <HomePage answer="Identifying risks" question="5) _________ involves prioritizing risks based on their probability and impact of occurrence."/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  overflow-x: hidden;
+}
+.heading_style{
+text-align:center;
+font-size: 36px;
+margin-left: 30% !important;
+margin-top: 30px;
+margin-bottom: 5%;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+     
+    <div class="row ml10">
+    <b-card bg-variant="secondary" text-variant="white">
+         {{question}}
+      </b-card>
+    </div><br>
+    <div class="row ml10">
+   
+     <b-button @click="revealAns()" variant="success" class="col-md-2">Click here for answer</b-button>
+     
+      <h3 v-if="ans" class="ml10 rounded border border-success" 
+                  style="background-color: darkseagreen;">{{ans}}</h3>
+     
+ 
+    </div>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data: function() {
+         return  {
+           ans: ''
+         }
+    },
+  props: {
+    question: String,
+    answer: String
+  },
+  methods: {
+    revealAns(){
+      this.ans= this.answer
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.ml10{
+  margin-left: 20px;
+}
+</style>
